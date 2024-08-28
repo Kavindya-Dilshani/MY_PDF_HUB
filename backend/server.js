2ef28742018c9248abad85e1b5fa4608bf4da980
Kavindya Dilshani <ranaweerakdilshani@gmail.com>
2024-08-28 11:29:46 +0530

    add env file

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,24 @@
+import dotenv from 'dotenv';
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import winston from "winston";
 import morgan from "morgan";
-import config from "./config/config.json" assert { type: "json" };
 import authRoutes from "./routes/auth.routes.js";
 import fileRoutes from "./routes/file.routes.js";
 
+// Environment Configuration
+dotenv.config();
 // Initialize Express application
 const app = express();
 
+const PORT = process.env.SERVER_PORT || 5000;
+const FRONT_END_URL = process.env.FRONT_END_URL;
+const DB_URL = process.env.DATABASE_URL;
+
 // CORS options
 const corsOptions = {
-  origin: [config.frontEndUrl],
+  origin: [FRONT_END_URL],
   methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD", "DELETE"],
   exposedHeaders: ['Content-Disposition'],
   credentials: true,
@@ -54,7 +60,7 @@ app.use(
 
 /** mongoDb connection */
 mongoose
-  .connect(config.databaseUrl)
+  .connect(DB_URL)
   .then(() => console.log("connected to MongoDB"))
   .catch((error) => console.log("failed to connect MongoDB:", error));
 
@@ -68,7 +74,6 @@ authRoutes(app);
 fileRoutes(app);
 
 // Set port, listen for requests
-const PORT = config.serverPort || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
